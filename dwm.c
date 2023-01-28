5569c2656a6a5176e1d27ddc331f650ec44d83fd
baduuz <62142988+baduuz@users.noreply.github.com>
2023-01-28 20:45:58 +0100

    Fixed merge issue

--- dwm.c
+++ dwm.c
@@ -252,7 +252,6 @@ static void seturgent(Client *c, int urg);
 static void show(const Arg *arg);
 static void showwin(Client *c);
 static void showhide(Client *c);
-static void sigchld(int unused);
 static void sighup(int unused);
 static void sigterm(int unused);
 static void spawn(const Arg *arg);
@@ -2193,14 +2192,6 @@ showhide(Client *c)
 		showhide(c->snext);
 		XMoveWindow(dpy, c->win, WIDTH(c) * -2, c->y);
 	}
-}
-
-void
-sigchld(int unused)
-{
-	if (signal(SIGCHLD, sigchld) == SIG_ERR)
-		die("can't install SIGCHLD handler:");
-	while (0 < waitpid(-1, NULL, WNOHANG));
 }
 
 void
